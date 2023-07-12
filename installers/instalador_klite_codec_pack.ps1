4cdeedfb15eafbea9abcc2ee4d42dad46dfbc031
Juan David Bogallo Quiroz <juanbogallo1113826@correo.itm.edu.co>
2023-07-12 17:42:13 -0500

    Actualizado - Instalador K-Lite Codec Pack
    Se ha añadido el script instalador de K-Lite Codec Pack - Opción 1 menú principal ARES Bionic-Flow Bot

---
+++ installers/instalador_klite_codec_pack.ps1
@@ -0,0 +1,25 @@
+$descargarMSEdge = Read-Host "¿Deseas instalar K-Lite Codec Pack? (S/N)"
+
+if ($descargarMSEdge -eq "S") {
+    Write-Host "¡Entendido! Comenzando tarea 1 de 2: Descarga de K-Lite Codec Pack..."
+
+    $url = "https://files3.codecguide.com/K-Lite_Codec_Pack_1768_Mega.exe"
+    $rutaDescarga = "C:\Users\Usuario-ROS\Downloads\K-Lite_Codec_Pack_1768_Mega.exe"
+
+    Invoke-WebRequest -Uri $url -OutFile $rutaDescarga
+
+    Write-Host "Tarea 1 de 2 - Descarga de K-Lite Codec Pack - ¡Completada!"
+    Write-Host "Comenzando tarea 2 de 2 - Instalación de K-Lite Codec Pack..."
+
+    # Ejecutar el instalador de K-Lite Codec Pack de forma silenciosa
+    Start-Process -FilePath $rutaDescarga -ArgumentList "/silent /install" -Wait
+
+    # Borrar el instalador descargado
+    Remove-Item $rutaDescarga -Force
+
+    Write-Host "¡Finalizado! K-Lite Codec Pack ha sido instalado correctamente." 
+    Write-Host "Ve al menú Inicio o abre la bandeja de íconos ocultos para comprobar que ya fue instalado :)"
+    Write-Host "¡Disfruta una calidad mejorada en tus archivos multimedia!"
+}
+
+$returnValue = "return-to-menu"
