4f9decf559ef0adcd2ce3d11a663ea2913b34cba
Juan David Bogallo Quiroz <juandavidbogallo2020@gmail.com>
2023-07-13 14:19:44 -0500

    Act. - Lógica instal. VLC arreglada
    Se ha actualizado el script instalador de VLC Media Player para que procese las instrucciones en caso de respuesta negativa (N/No) o respuestas inválidas, además de corregir ciertas variables nombradas incorrectamente.

--- installers/instalador_vlc_media_player.ps1
+++ installers/instalador_vlc_media_player.ps1
@@ -1,6 +1,6 @@
-$descargarMSEdge = Read-Host "¿Deseas instalar VLC Media Player? (S/N)"
+$descargarVLCMP = Read-Host "¿Deseas instalar VLC Media Player? (S/N)"
 
-if ($descargarMSEdge -eq "S") {
+if ($descargarVLCMP -eq "S") {
     Write-Host "¡Entendido! Comenzando tarea 1 de 2: Descarga de VLC Media Player..."
 
     $url = "https://edgeuno-bog2.mm.fcix.net/videolan-ftp/vlc/3.0.18/win64/vlc-3.0.18-win64.exe"
@@ -19,6 +19,19 @@ if ($descargarMSEdge -eq "S") {
 
     Write-Host "¡Finalizado! VLC Media Player ha sido instalado correctamente." 
     Write-Host "Ve al menú Inicio para comprobar que ya fue instalado :)"
+    Write-Host "Refrescando el menú..."
+    Start-Sleep -Seconds 5
+    $returnValue
+    exit
 }
-
+elseif ($descargarVLCMP -eq "N"){
+    Write-Host "De acuerdo. Refrescando el menú..."
+    Start-Sleep -Seconds 5
+    $returnValue
+    exit
+}
+else{
+    Write-Host "Disculpa, no entendí tu respuesta. ¡Inténtalo de nuevo!"
+    Write-Host ($descargarVLCMP)
+}
 $returnValue = "return-to-menu"
