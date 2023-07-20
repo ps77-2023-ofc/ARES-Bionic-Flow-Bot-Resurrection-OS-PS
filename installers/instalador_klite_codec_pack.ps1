de9b8e0e7a219aaf327ffc1f7b21491e38c550f1
Juan David Bogallo Quiroz <juandavidbogallo2020@gmail.com>
2023-07-20 17:48:05 -0500

    Añadido - Script instalador KLCP
    Se añade el nuevo script instalador de K-Lite Codec Pack - (Menú Principal > Opción 1 > Opción 6)

---
+++ installers/instalador_klite_codec_pack.ps1
@@ -0,0 +1,38 @@
+$descargarKLCP = Read-Host "¿Deseas instalar K-Lite Codec Pack? (S/N)"
+
+if ($descargarKLCP -eq "S") {
+    Write-Host "¡Entendido! Comenzando tarea 1 de 2: Descarga de K-Lite Codec Pack..."
+
+    $url = "https://files3.codecguide.com/K-Lite_Codec_Pack_1768_Mega.exe"
+    $rutaDescarga = "C:\Users\Usuario-ROS\Downloads\K-Lite_Codec_Pack_1768_Mega.exe"
+
+    Invoke-WebRequest -Uri $url -OutFile $rutaDescarga -Quiet
+
+    Write-Host "Tarea 1 de 2 - Descarga de K-Lite Codec Pack - ¡Completada!"
+    Write-Host "Comenzando tarea 2 de 2 - Instalación de K-Lite Codec Pack..."
+
+    # Ejecutar el instalador de K-Lite Codec Pack de forma silenciosa
+    Start-Process -FilePath $rutaDescarga -ArgumentList "/silent /install" -Wait
+
+    # Borrar el instalador descargado
+    Remove-Item $rutaDescarga -Force
+
+    Write-Host "¡Finalizado! K-Lite Codec Pack ha sido instalado correctamente." 
+    Write-Host "Ve al menú Inicio o abre la bandeja de íconos ocultos para comprobar que ya fue instalado :)"
+    Write-Host "¡Disfruta una calidad mejorada en tus archivos multimedia!"
+    Write-Host "Refrescando el menú..."
+    Start-Sleep -Seconds 5
+    #Establecer variable de entorno para indicar el regreso, una vez terminada la instalación.
+    $env:InstaladorKLCPEstado = "regresar-al-menu"
+}
+elseif ($descargarKLCP -eq "N"){
+    Write-Host "De acuerdo. Refrescando el menú..."
+    Start-Sleep -Seconds 5
+    #Establecer variable de entorno para indicar el regreso, una vez terminada la instalación.
+    $env:InstaladorKLCPEstado = "regresar-al-menu"
+}
+else{
+    Write-Host "Disculpa, no entendí tu respuesta. ¡Inténtalo de nuevo!"
+    Write-Host ($descargarKLCP)
+}
+
