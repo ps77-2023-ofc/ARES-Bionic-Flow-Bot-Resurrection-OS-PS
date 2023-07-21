7bcb9c0742e17a21e5620b5dfacf48a50702cd56
Juan David Bogallo Quiroz <juandavidbogallo2020@gmail.com>
2023-07-21 13:55:44 -0500

    Añadido - T.Misceláneas - Casos Incompat
    Se añaden los scripts tareas misceláneas que imprimen resultados en caso de encontrarse en los casos 1-8 de incompatibilidad con Lively Wallpaper.

---
+++ miscellaneous/check_results/lively_wallpaper/incompat_error_cases/lively_error_case_2.ps1
@@ -0,0 +1,14 @@
+function Assert-Error-Case-2 {
+    Write-Host "¡Terminé! Aquí tengo tus resultados:"
+    Write-Host "Estado de la compatibilidad: NO CUMPLES LOS REQUISITOS: CASO 2."
+    Write-Host "Hmm... parece que tu equipo no cumple con los requisitos mínimos/recomendados. En base a mis análisis, aplicas al Caso #2 de incompatibilidad."
+    Write-Host "Ve a la Base de Conocimientos oficial de Resurrection OS y chequea el caso al que aplicas para saber qué he encontrado."
+    Write-Host "Puedes acceder a ella en: https://docs-resurrectionos.gitbook.io/resurrection-os-knowledge-base/usando-al-ares-bot/opcion-8-lively-wallpaper#casos-de-incompatibilidad"
+    Start-Sleep -Seconds 5
+    Write-Host "Independientemente del caso en el que te encuentres, aún puedo ayudarte a instalar Lively Wallpaper, pero será bajo tu propio riesgo."
+    Write-Host "Si decides instalar Lively Wallpaper de todas maneras, y empiezas a experimentar problemas de rendimiento, y si al usar"
+    Write-Host "mis herramientas de optimización no ves mejoría o ves muy poca, mi creador no tomará responsabilidad ante ello. Quedas advertido."
+}
+Assert-Error-Case-2
+$LWcase2DisclaimerAccepted = Read-Host "Dicho esto, ¿quieres continuar de todos modos? (S/N)"
+$env:LivelyIncompatCaso2Advert = $LWcase2DisclaimerAccepted
