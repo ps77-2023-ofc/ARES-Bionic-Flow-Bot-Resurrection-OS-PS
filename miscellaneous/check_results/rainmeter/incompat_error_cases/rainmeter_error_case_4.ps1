af503163ad5695c3e02b954d76a5766990cf2d35
Juan David Bogallo Quiroz <juandavidbogallo2020@gmail.com>
2023-07-21 14:09:34 -0500

    Añadido - Tareas misceláneas Rainmeter

---
+++ miscellaneous/check_results/rainmeter/incompat_error_cases/rainmeter_error_case_4.ps1
@@ -0,0 +1,14 @@
+function Assert-Error-Case-4 {
+    Write-Host "¡Terminé! Aquí tengo tus resultados:"
+    Write-Host "Estado de la compatibilidad: NO CUMPLES LOS REQUISITOS: CASO 4."
+    Write-Host "Hmm... parece que tu equipo no cumple con los requisitos mínimos/recomendados. En base a mis análisis, aplicas al Caso #4 de incompatibilidad."
+    Write-Host "Ve a la Base de Conocimientos oficial de Resurrection OS y chequea el caso al que aplicas para saber qué he encontrado."
+    Write-Host "Puedes acceder a ella en: https://docs-resurrectionos.gitbook.io/resurrection-os-knowledge-base/usando-al-ares-bot/opcion-6-rainmeter#casos-de-incompatibilidad"
+    Start-Sleep -Seconds 5
+    Write-Host "Independientemente del caso en el que te encuentres, aún puedo ayudarte a instalar Rainmeter, pero será bajo tu propio riesgo."
+    Write-Host "Si decides instalar Rainmeter de todas maneras, y empiezas a experimentar problemas de rendimiento, y si al usar"
+    Write-Host "mis herramientas de optimización no ves mejoría o ves muy poca, mi creador no tomará responsabilidad ante ello. Quedas advertido."
+}
+Assert-Error-Case-4
+$case4DisclaimerAccepted = Read-Host "Dicho esto, ¿quieres continuar de todos modos? (S/N)"
+$env:RainmeterIncompatibleCaso4AdvertAceptada = $case4DisclaimerAccepted
